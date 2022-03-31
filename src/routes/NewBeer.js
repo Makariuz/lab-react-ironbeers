07dcd472c847b8a631e292397cc277b7c5712534
Makariuz <samuel.eduardo23@hotmail.com>
2022-03-31 16:02:16 +0100

    failed attempt to fix add new beer

--- src/routes/NewBeer.js
+++ src/routes/NewBeer.js
@@ -3,8 +3,31 @@ import './NewBeer.scss'
 import {IoIosAddCircleOutline} from 'react-icons/io'
 import { useState, useEffect } from "react"
 import { axios } from "axios"
+import { useParams } from "react-router-dom";
+
 
 export function NewBeer(){
+
+    const [beer, setBeer] = useState([])
+
+    const { beerId } = useParams()
+
+    useEffect(() => {
+        fetch('https://ih-beers-api2.herokuapp.com/beers')
+        .then(res => res.json())
+        .then(json=> setBeer(json))
+    },[])
+
+      useEffect(() => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+          };
+        fetch('https://ih-beers-api2.herokuapp.com/beers/new', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ id: data.id }));
+      },[])
+
 
     const [newBeer, setNewBeer] = useState('')
     const [newTagline, setNewTagline] = useState('')
@@ -14,7 +37,7 @@ export function NewBeer(){
     const [newAttenuationLevel, setNewAttenuationLevel] = useState('')
     const [newContributedBy, setNewContributedBy] = useState('')
 
-    const handleSubmit = async (e) => {
+  /*   const handleSubmit = async (e) => {
         e.preventDefault();
         const data = {
             newBeer,
@@ -29,12 +52,40 @@ export function NewBeer(){
    
         const newBeerInFO = await axios.post(url, data);
         console.log(newBeerInFO)
-    }
+    } */
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setBeer((previousInput) => {
+          return [
+            ...previousInput,
+            {
+              beer: newBeer,
+              tagline: newTagline,
+              description: newDescription,
+              first_brewed: newFirstBrewed,
+              brewers_tips: newBrewerTips,
+              attenuation_level: newAttenuationLevel,
+              contributed_by:newContributedBy,
+            },
+          ];
+        });
+        console.log(beer)
+        setNewBeer('')
+        setNewTagline('')
+        setNewDescription('')
+        setNewFirstBrewed('')
+        setNewAttenuationLevel('')
+        setNewContributedBy('')
+
+      };
+
+
     return (
-        <div onSubmit={handleSubmit} className="container">
+        <div className="container">
         <LinkHome />
         <h2>Add a new beer</h2>
-        <form className="form-input">
+        <form onSubmit={handleSubmit} className="form-input">
         <input type="text" placeholder="name" value={newBeer} onChange={(event) => setNewBeer(event.target.value)}/>
         <input type="text" placeholder="tagline" value={newTagline} onChange={(event) => setNewTagline(event.target.value)}/>
         <input type="text" placeholder="description" value={newDescription} onChange={(event) => setNewDescription(event.target.value)} />
